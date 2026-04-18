fd3fd618e68584fbe5dc2c872ce93e1fc4a38618
qqq142 <nekrasova.valeriia@mail.ru>
2026-04-18 20:55:38 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int arrSize{};
+    std::cout << "¬ведите размер массива: ";
+    std::cin >> arrSize;
+
+    int * intPtr = static_cast<int *>(malloc(arrSize * sizeof (int)));
+
+    for (int i = 0; i < arrSize; i++) {
+        std::cout << "arr[" << i << "] = ";
+        std::cin >> intPtr[i];
+    }
+
+    std::cout << "¬ведЄнный массив: ";
+    for (int i = 0; i < arrSize; i++) {
+        std::cout<< intPtr[i] << " ";
+    }
+
+    free(intPtr);
+
+    return 0;
+}
